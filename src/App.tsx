2dfda0f71439b7aea88426ac53b762bc8b690bf0
Victor Maina <contact@victormaina.com>
2023-02-01 02:06:16 +0300

    make aside responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import Main from "./Main/Main";
 import Aside from "./Aside/Aside";
 import Background from "./Background/Background";
+import styles from "./app.module.scss";
 
 export default function App() {
   return (
     <>
-      <Main />
-      <Aside />
+      <div /* style={{transform: 'translateX(425px)'}} */ className={styles["outer-wrapper"]}>
+        <Aside />
+        <Main />
+      </div>
       <Background />
     </>
   );
--- src/Aside/Aside.tsx
+++ src/Aside/Aside.tsx
@@ -4,11 +4,9 @@ import styles from "./aside.module.scss";
 
 export default function Aside() {
   return (
-    <aside className={styles["aside-hidden"]}>
-      <div className={styles["aside-inner"]}>
-        <ImageForm />
-        <SavedImages />
-      </div>
+    <aside className={styles["aside"]}>
+      <ImageForm />
+      <SavedImages />
     </aside>
   );
 }
