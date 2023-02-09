e4e5dfdeb2c2ba3ce0b57cc770bbe434c9ffaca2
Victor Maina <contact@victormaina.com>
2023-02-10 00:43:35 +0300

    comment out ImageCollection

--- src/models/Image/ImageCollection.class.ts
+++ src/models/Image/ImageCollection.class.ts
@@ -1,107 +1,106 @@
-import {
-  EmptyImageURLError,
-  ImageURLExistsError,
-  InvalidImageURLError,
-  NoLocalStorageImageCollection,
-} from "./ImageErrors.class";
+// import {
+//   EmptyImageURLError,
+//   ImageURLExistsError,
+//   InvalidImageURLError,
+//   NoLocalStorageImageCollection,
+// } from "./ImageErrors.class";
 
-import { Image } from "./Image.class";
-import ImageInterface from "./Image.interface";
+// import { Image } from "./Image.interface";
 
 export class ImageCollection {
-  constructor(collectionName = "images") {
-    this.name = collectionName;
-    this.images = [];
+  // constructor(collectionName = "images") {
+  //   this.name = collectionName;
+  //   this.images = [];
 
-    // Check local storage for images and initialize collection with them
-    const imagesLocalStorage = localStorage.getItem(collectionName);
-    if (imagesLocalStorage) {
-      const parsedImages: ImageInterface[] = JSON.parse(imagesLocalStorage);
-      if (parsedImages.length > 0) {
-        parsedImages.forEach((image) => {
-          this.createImage(image.imageUrl, image.description, image.id);
-        });
-      }
-    } else {
-      // Create new localstorage key on initialization
-      this.updateLocalStorage();
-    }
-  }
+  //   // Check local storage for images and initialize collection with them
+  //   const imagesLocalStorage = localStorage.getItem(collectionName);
+  //   if (imagesLocalStorage) {
+  //     const parsedImages: Image[] = JSON.parse(imagesLocalStorage);
+  //     if (parsedImages.length > 0) {
+  //       parsedImages.forEach((image) => {
+  //         this.createImage(image.imageUrl, image.description, image.id);
+  //       });
+  //     }
+  //   } else {
+  //     // Create new localstorage key on initialization
+  //     this.updateLocalStorage();
+  //   }
+  // }
 
-  name: string;
+  // name: string;
 
-  private images: Image[];
+  // private images: Image[];
 
-  private updateLocalStorage(): void {
-    localStorage.setItem(this.name, JSON.stringify(this.images));
-  }
+  // private updateLocalStorage(): void {
+  //   localStorage.setItem(this.name, JSON.stringify(this.images));
+  // }
 
-  private generateID(length: number): string {
-    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let id = "";
+  // private generateID(length: number): string {
+  //   const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //   let id = "";
 
-    while (true) {
-      for (let i = 0; i < length; i++) {
-        id += chars[Math.floor(Math.random() * chars.length)];
-      }
+  //   while (true) {
+  //     for (let i = 0; i < length; i++) {
+  //       id += chars[Math.floor(Math.random() * chars.length)];
+  //     }
 
-      if (this.images.every((image) => image.id !== id)) break;
-    }
+  //     if (this.images.every((image) => image.id !== id)) break;
+  //   }
 
-    return id;
-  }
+  //   return id;
+  // }
 
-  get allImages(): Image[] {
-    return this.images;
-  }
+  // get allImages(): Image[] {
+  //   return this.images;
+  // }
 
-  public validateUrl(imageUrl: string): void {
-    // Check for empty string
-    if (!imageUrl) throw new EmptyImageURLError();
+  // public validateUrl(imageUrl: string): void {
+  //   // Check for empty string
+  //   if (!imageUrl) throw new EmptyImageURLError();
 
-    // Check if url begins with http protocol
-    try {
-      const newUrl = new URL(imageUrl);
-      if (newUrl.protocol !== "http:" && newUrl.protocol !== "https:") {
-        throw new InvalidImageURLError();
-      }
-    } catch (err) {
-      throw new InvalidImageURLError();
-    }
+  //   // Check if url begins with http protocol
+  //   try {
+  //     const newUrl = new URL(imageUrl);
+  //     if (newUrl.protocol !== "http:" && newUrl.protocol !== "https:") {
+  //       throw new InvalidImageURLError();
+  //     }
+  //   } catch (err) {
+  //     throw new InvalidImageURLError();
+  //   }
 
-    // Check if url already exists in image collection
-    const existingImage = this.images.find(
-      (image) => image.imageUrl === imageUrl
-    );
-    if (existingImage) {
-      throw new ImageURLExistsError(existingImage);
-    }
+  //   // Check if url already exists in image collection
+  //   const existingImage = this.images.find(
+  //     (image) => image.imageUrl === imageUrl
+  //   );
+  //   if (existingImage) {
+  //     throw new ImageURLExistsError(existingImage);
+  //   }
 
-    // Check if images exists in localStorage
-    const imagesLocalStorage = localStorage.getItem("images");
-    if (!imagesLocalStorage) throw new NoLocalStorageImageCollection();
-  }
+  //   // Check if images exists in localStorage
+  //   const imagesLocalStorage = localStorage.getItem("images");
+  //   if (!imagesLocalStorage) throw new NoLocalStorageImageCollection();
+  // }
 
-  public createImage(
-    imageUrl: string,
-    description: string,
-    id?: string
-  ): Image {
-    try {
-      this.validateUrl(imageUrl);
+  // public createImage(
+  //   imageUrl: string,
+  //   description: string,
+  //   id?: string
+  // ): Image {
+  //   try {
+  //     this.validateUrl(imageUrl);
 
-      const image = new Image(imageUrl, description);
+  //     const image = new Image(imageUrl, description);
 
-      if (id) image.id = id;
-      else image.id = this.generateID(8);
+  //     if (id) image.id = id;
+  //     else image.id = this.generateID(8);
 
-      this.images.push(image);
+  //     this.images.push(image);
 
-      this.updateLocalStorage();
+  //     this.updateLocalStorage();
 
-      return image;
-    } catch (error) {
-      throw error;
-    }
-  }
+  //     return image;
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // }
 }
